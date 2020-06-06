433ad79e7570b891462023a71108a61b9c8eefa1
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-06 11:35:56 +0700

    tambah column username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('role_id')->default(3);
             $table->string('name');
-            $table->string('username',100);
+            $table->string('username',100)->unique();
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 100);
