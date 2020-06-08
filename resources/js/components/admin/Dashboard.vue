821ae4fb50405bb3ecc6815879232160239b4bf5
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-08 19:56:40 +0700

    menu admin, produk dan sales fix

---
+++ resources/js/components/admin/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-col cols lg="10" xs=12>
+    <div class="login-form mx-auto text-center">
+        <h2>Menu Admin</h2>
+        <router-link to="/master-harga" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data Harga</b-button></router-link>
+        <router-link to="/master-sales" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data Sales</b-button></router-link>
+        <router-link to="/master-user" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data User</b-button></router-link>
+        <router-link to="/master-produk" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data Produk</b-button></router-link>
+        <router-link to="/master-dc" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data DC</b-button></router-link>       
+    </div>
+  </b-col>
+</template>
+<script>
+  
+  export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        
+    }
+  }
+
+</script>
