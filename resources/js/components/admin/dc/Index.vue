202f678a8194eff6ab7b9da05522e78709f32dab
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-08 19:59:22 +0700

    master tabel produk

---
+++ resources/js/components/admin/dc/Index.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-col cols lg="10" xs=12>
+    <div class="login-form mx-auto text-center">
+      <h2>Master Data Produk</h2>
+      <router-link :to="{name: 'create-produk'}" class="btn btn-success">Produk Baru</router-link>
+      <v-client-table :data="tableProduk" :columns="columns" :options="options">
+        <router-link :to="{name: 'edit-produk', params: { id: props.row.id }}" slot="edit" slot-scope="props"><b-button size="sm" variant="outline-primary">Edit</b-button></router-link>
+        <b-button size="sm" variant="outline-danger" @click="deleteProduk(props.row.id)"  slot="hapus" slot-scope="props">Delete</b-button>
+      </v-client-table>
+    </div>
+  </b-col>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        dataProduk: [],
+        columns: ['id', 'nama_produk', 'keterangan', 'created_at', 'updated_at', 'edit', 'hapus'],
+        tableProduk: [
+          //{
+            //id: this.id, title: this.title, meta:this.meta, edit: this.created_at
+          //}
+        ],
+        options: {
+          perPage: 10,
+          headings: {
+            title: 'Judul',
+            nama_produk: 'Nama Produk',
+            keteragan: 'Keterangan',
+            created_at: 'Dibuat',
+            updated_at: 'Diperbarui',
+            edit:'Ubah',
+            hapus: 'Hapus'
+          },
+          sortable: ['id', 'nama_produk', 'created_at', 'updated_at']
+        }
+      }
+    },
+    created() {
+      this.axios
+        .get('/api/produk')
+        .then(response => {
+          this.dataProduk = response.data.produk
+          this.tableProduk = response.data.produk
+        })
+    },
+    methods: {
+      deleteProduk(id) {
+        this.axios
+          .delete(`/api/produk/delete/${id}`)
+          .then(response => {
+            let i = this.dataProduk.map(item => item.id).indexOf(id); // find index of your object
+            this.dataProduk.splice(i, 1)
+          })
+      }
+    }
+  }
+</script>
