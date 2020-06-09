adaeb1fcaf4ba5354754dba0e1bdb3e9d2fd0fe8
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-09 12:47:22 +0700

    test route akun dan dc

--- routes/api.php
+++ routes/api.php
@@ -16,27 +16,37 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route untuk auth via api passport
 Route::post('login', 'Api\UserController@login');
 Route::post('logout', 'Api\UserController@logout');
 Route::post('register', 'Api\UserController@register');
 
-// Route api produk
-Route::get('produk', 'ProdukController@index')->middleware('auth:api');
-Route::post('produk/store', 'ProdukController@store');
-Route::get('produk/edit/{id}', 'ProdukController@edit');
-Route::post('produk/update/{id}', 'ProdukController@update');
-Route::delete('produk/delete/{id}', 'ProdukController@destroy');
+// Kumpulan route yang diprotect oleh middleware api
+Route::group(['middleware' => ['auth:api']], function(){
+    // Route api produk
+    Route::get('produk', 'ProdukController@index');
+    Route::post('produk/store', 'ProdukController@store');
+    Route::get('produk/edit/{id}', 'ProdukController@edit');
+    Route::post('produk/update/{id}', 'ProdukController@update');
+    Route::delete('produk/delete/{id}', 'ProdukController@destroy');
 
-// Route api sales
-Route::get('sales', 'SalesController@index');
-Route::post('sales/store', 'SalesController@store');
-Route::get('sales/edit/{id}', 'SalesController@edit');
-Route::post('sales/update/{id}', 'SalesController@update');
-Route::delete('sales/delete/{id}', 'SalesController@destroy');
+    // Route api sales
+    Route::get('sales', 'SalesController@index');
+    Route::post('sales/store', 'SalesController@store');
+    Route::get('sales/edit/{id}', 'SalesController@edit');
+    Route::post('sales/update/{id}', 'SalesController@update');
+    Route::delete('sales/delete/{id}', 'SalesController@destroy');
 
-Route::get('auth-check', function() {
-    return Auth::check();
+    // Route api dc
+    Route::get('dc', 'DCController@index');
+    Route::post('dc/store', 'DCController@store');
+    Route::get('dc/edit/{id}', 'DCController@edit');
+    Route::post('dc/update/{id}', 'DCController@update');
+    Route::delete('dc/delete/{id}', 'DCController@destroy');
+
 });
-Route::group(['middleware' => 'auth:api'], function(){
-    Route::post('details', 'API\UserController@details');
-});
+
+Route::get('akun', 'Api\UserController@index');
+Route::get('akun/edit/{id}', 'Api\UserController@edit');
+Route::get('kode_dc', 'DCController@kode_dc');
