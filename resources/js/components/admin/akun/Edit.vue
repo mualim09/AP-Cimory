faf8fe3c7f2a3923c4d8dabd82ba1849870dc207
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-09 16:12:28 +0700

    fix bug kode_dc -> dc_id

--- resources/js/components/admin/akun/Edit.vue
+++ resources/js/components/admin/akun/Edit.vue
@@ -27,20 +27,20 @@
           <input id="password" type="password" class="form-control" name="password" required v-model="user.password">
           </div>
         </div>
-        <div class="form-group row">
+        <!-- <div class="form-group row">
           <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
           <div class="col-md-6">
           <input id="password-confirm" type="password" class="form-control" name="password-confirm" required v-model="user.password_confirmation">
           </div>
-        </div>
+        </div> -->
         <div class="form-group row">
-          <label for="DC" class="col-md-4 col-form-label text-md-right">Posisi DC</label>
+          <label for="DC" class="col-md-4 col-form-label text-md-right">Posisi</label>
           <div class="col-md-6">
-          <select v-model="user.kode_dc" class="form-control">
+          <select v-model="user.dc_id" class="form-control">
             <option value="ho">HO</option>
             <option value="admin">Admin</option>
-            <option v-for="dc in kode_dc" :value="dc.kode_dc" :key="dc.kode_dc">
-              {{ dc.kode_dc }} {{ dc.nama_dc }}
+            <option v-for="dc in kode_dc" :value="dc.id" :key="dc.id">
+              {{ dc.kode_dc }} - {{ dc.nama_dc }}
             </option>
           </select>
           </div>
@@ -62,7 +62,6 @@
     data() {
       return {
         user: {},
-        auth: null,
         kode_dc: []
       }
     },
@@ -87,14 +86,8 @@
 
     methods: {
       updateAkun() {
-        const formData = new FormData();
-        formData.append('name', this.user.name);
-        formData.append('username', this.user.username);
-        formData.append('email', this.user.email);
-        formData.append('password', this.user.password);
-        formData.append('password_confirmation', this.user.password_confirmation);
         this.axios
-          .post('api/register', formData)
+          .post(`/api/akun/update/${this.$route.params.id}`, this.user)
           .then(response => (
             console.log(response.data),
             this.$router.push({name: 'master-akun'})
