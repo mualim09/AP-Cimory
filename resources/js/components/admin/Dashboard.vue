9f56dca438fc29cc3ea6db084a1a5d621966ec04
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-09 12:45:18 +0700

    master akun, gunakan keyword akun

--- resources/js/components/admin/Dashboard.vue
+++ resources/js/components/admin/Dashboard.vue
@@ -4,7 +4,7 @@
         <h2>Menu Admin</h2>
         <router-link to="/master-harga" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data Harga</b-button></router-link>
         <router-link to="/master-sales" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data Sales</b-button></router-link>
-        <router-link to="/master-user" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data User</b-button></router-link>
+        <router-link to="/master-akun" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data Akun/User</b-button></router-link>
         <router-link to="/master-produk" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data Produk</b-button></router-link>
         <router-link to="/master-dc" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="box-arrow-in-right"></b-icon> Master Data DC</b-button></router-link>       
     </div>
