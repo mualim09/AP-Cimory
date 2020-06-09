36555c765b99540d4be97b2825b56d837fa80d74
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-09 12:42:59 +0700

    index data user, konfirmasi password

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,65 +7,87 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
 
     public $successStatus = 200;
+    
+    public function index()
+    {
+        // $akun = User::all()->toArray();
+        $akun = User::join('dc', 'users.dc_id', '=', 'dc.id')
+            ->join('role', 'users.role_id', '=', 'role.id')
+            ->select('users.id', 'users.name', 'users.username', 'role.role', 'dc.kode_dc', 'users.created_at', 'users.updated_at')
+            ->get();
+        // $akun = User::doesntHave('dc_id')->orWhereHas('dc_id', function($query) {
+        //     $query->join('dc', 'users.dc_id', '=', 'dc.id')
+        //     ->join('role', 'users.role_id', '=', 'role.id')
+        //     ->select('users.id', 'users.name', 'users.username', 'role.role', 'dc.kode_dc', 'users.created_at', 'users.updated_at')
+        //     ->get();
+        // });
+        return response()->json([
+        'akun' => $akun,
+        'success' => 'sukses ambil data akun'
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $akun = User::find($id)->toArray();
+        return response()->json($akun);
+    }
 
     public function login(Request $request){
-        // if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
-        //     $user = Auth::user();
-        //     $success['token'] =  $user->createToken('nApp')->accessToken;
-        //     return response()->json(['success' => $success], $this->successStatus);
-        // }
-        // else{
-        //     return response()->json(['error'=>'Unauthorised'], 401);
-        // }
+        // Validasi username dan password
         $this->validate($request, [
-            'username' => 'required|string', //VALIDASI KOLOM USERNAME
-            //TAPI KOLOM INI BISA BERISI EMAIL ATAU USERNAME
+            'username' => 'required|string',
             'password' => 'required|string|min:6',
         ]);
 
-        //LAKUKAN PENGECEKAN, JIKA INPUTAN DARI USERNAME FORMATNYA ADALAH EMAIL, MAKA KITA AKAN MELAKUKAN PROSES AUTHENTICATION MENGGUNAKAN EMAIL, SELAIN ITU, AKAN     MENGGUNAKAN USERNAME
+        // Otomatis tipe login dengan username atau email, keduanya bisa walaupun form input username
         $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
       
-        //TAMPUNG INFORMASI LOGINNYA, DIMANA KOLOM TYPE PERTAMA BERSIFAT DINAMIS BERDASARKAN VALUE DARI PENGECEKAN DIATAS
+        // Simpan informasi loginnya
         $login = [
             $loginType => $request->username,
             'password' => $request->password
         ];
       
-        //LAKUKAN LOGIN
+        // Proses login
         if (Auth::attempt($login)) {
-            //JIKA BERHASIL, MAKA REDIRECT KE HALAMAN HOME
-            // return redirect()->route('home');
+            // Jika berhasil berikan respon username, role dan token yang akan disimpan pada state vuex dan localStorage rowser
             $token = Auth::user()->createToken('nApp')->accessToken;
             $username = Auth::user()->username;
             $role = Auth::user()->role['role'];
             $status_auth = Auth::check();
             return response()->json(['token' => $token, 'username' => $username, 'role' => $role, 'status' => $status_auth]);
         }
-        //JIKA SALAH, MAKA KEMBALI KE LOGIN DAN TAMPILKAN NOTIFIKASI 
-        return redirect()->route('/')->with(['error' => 'Email/Password salah!']);
+        // Jika salah berikan informasi error 
+        return response()->json('username atau password salah');
     }
 
     public function register(Request $request)
     {
+        // Validasi semua form input
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
+            'username' => 'alpha_dash|required|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6',
+            // 'password-confirm' => 'required|same:password',
+            'dc_id'
         ]);
 
         if ($validator->fails()) {
             return response()->json(['error'=>$validator->errors()], 401);            
         }
 
+        // Proses registrasi 
         $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
+        $input['password'] = Hash::make($input['password']);
+        // Buat akun baru
         $user = User::create($input);
         $success['token'] =  $user->createToken('nApp')->accessToken;
         $success['name'] =  $user->name;
@@ -77,7 +99,6 @@ class UserController extends Controller
     {   
 	    if (Auth::check()) {
             Auth::logout();
-            // $status_auth = Auth::check();
 	        return response()->json(['success' =>'logout_success']); 
 	    } else {
 	        return response()->json(['error' => 'anda tidak login']);
