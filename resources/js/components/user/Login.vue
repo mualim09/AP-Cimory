abdf0cc8dd581b73120166af91e6cbe0cb03ad7f
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-11 07:50:34 +0700

    bersihkan coding, tampilkan error

--- resources/js/components/user/Login.vue
+++ resources/js/components/user/Login.vue
@@ -2,54 +2,48 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-10 mx-auto">
-      <div class="" v-if="!auth">
-        <form @submit.prevent="login">
-          <div class="form-group row">
-            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
-            <div class="col-md-6">
-            <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" required autofocus v-model="user.username">
-              <span class="invalid-feedback" role="alert">
-              <strong></strong>
-              </span>
+        <div class="" >
+          <form @submit.prevent="login">
+            <div class="form-group row">
+              <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+              <div class="col-md-6">
+              <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" required autofocus v-model="user.username">
+                <span class="invalid-feedback" role="alert">
+                <strong></strong>
+                </span>
+              </div>
             </div>
-          </div>
 
-          <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-            <div class="col-md-6">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required v-model="user.password">
-              <span class="invalid-feedback" role="alert">
-              <strong></strong>
-              </span>
+            <div class="form-group row">
+              <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+              <div class="col-md-6">
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required v-model="user.password">
+                <span class="invalid-feedback" role="alert">
+                <strong></strong>
+                </span>
+              </div>
             </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-md-6 offset-md-4">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="remember" id="remember" >
-              <label class="form-check-label" for="remember">
-              Remember Me
-              </label>
+            <div class="form-group row">
+              <div class="col-md-6 offset-md-4">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember" >
+                <label class="form-check-label" for="remember">
+                Remember Me
+                </label>
+              </div>
+              </div>
             </div>
-            </div>
-          </div>
 
-          <div class="form-group row mb-0">
-            <div class="col-md-8 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-              Login
-            </button>
+            <div class="form-group row mb-0">
+              <div class="col-md-8 offset-md-4">
+              <button type="submit" class="btn btn-primary">
+                Login
+              </button>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
-      <div  v-if="auth">
-        <form id="logout-form" @submit.prevent="logout" style="">
-        <button type="submit" class="btn btn-sm btn-primary">
-          Logout
-        </button>
-        </form>
-      </div>
+          </form>
+          <p v-if="errorLogin" class="text-danger">{{ errorLogin }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -59,7 +53,7 @@
   data() {
     return {
     user: {},
-    auth: false
+    errorLogin: null,
     }
   },
   methods: {
@@ -78,7 +72,10 @@
       this.$router.push({name: `${response.data.role}-dashboard`}),
       console.log(response.data)
       ))
-      .catch(error => console.log(error))
+      .catch(error => (
+        this.errorLogin = error.response.data,
+        console.log(this.errorLogin)
+      ))
       .finally(() => this.loading = false);
     },
     logout(){
@@ -91,7 +88,8 @@
         this.$router.push({name: 'login'}),
         this.auth = false
       ))
-      .catch(error => console.log(error))
+      .catch(error => (console.log(error))
+      )
       .finally(() => this.loading = false)
     }
   },
