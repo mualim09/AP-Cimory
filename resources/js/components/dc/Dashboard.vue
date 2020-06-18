db0e3dbc552c2bc8319991693e3854ba9d598f22
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-18 09:23:37 +0700

    tambah menu dc

--- resources/js/components/dc/Dashboard.vue
+++ resources/js/components/dc/Dashboard.vue
@@ -2,17 +2,21 @@
   <b-col cols lg="10" xs=12>
   <div class="login-form mx-auto text-center">
     <h2>Menu DC</h2>
-    <router-link to="/create-transaksi" class="nav-item nav-link"><b-button class="btn-landing" variant="success"><b-icon icon="tag"></b-icon> Input Laporan</b-button></router-link>
-    <!-- <router-link to="/edit-laporan-dc" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="people"></b-icon> Master Data Sales</b-button></router-link>-->
-    <router-link to="/master-laporan-dc" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="person"></b-icon> Laporan DC</b-button></router-link>       
+    <router-link to="/create-pembelian" class="nav-item nav-link"><b-button class="btn-landing" variant="success"><b-icon icon="tag"></b-icon>Pembelian</b-button></router-link>
+    <router-link to="/create-penjualan" class="nav-item nav-link"><b-button class="btn-landing" variant="success"><b-icon icon="tag"></b-icon>Penjualan</b-button></router-link>
+    <router-link to="/create-retur" class="nav-item nav-link"><b-button class="btn-landing" variant="success"><b-icon icon="tag"></b-icon>Retur</b-button></router-link>
+    <router-link to="/master-pembelian" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="person"></b-icon>Laporan Pembelian</b-button></router-link>
+    <router-link to="/master-penjualan" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="person"></b-icon>Laporan Penjualan</b-button></router-link>
+    <router-link to="/master-retur" class="nav-item nav-link"><b-button class="btn-landing" variant="primary"><b-icon icon="person"></b-icon>Laporan Retur</b-button></router-link>
+    <router-link to="/master-stok" class="nav-item nav-link"><b-button class="btn-landing" variant="warning"><b-icon icon="person"></b-icon>Laporan Stok</b-button></router-link>
   </div>
   </b-col>
 </template>
 <script>
-  
+
 export default {
   components: {
-    
+
   },
   data() {
     return {
@@ -24,8 +28,8 @@ export default {
 
   },
   methods: {
-    
+
   }
 }
-  
+
 </script>
