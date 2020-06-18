3b6712fd60bc5c9655d9e7c8a9b397e768dbdd52
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-18 09:22:28 +0700

    untuk migration baru ubah nama filenya

---
+++ database/migrations/2020_06_16_135653x_stok.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Stok extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('stok', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->date('tanggal_stok');
+          $table->string('keterangan');
+          $table->unsignedBigInteger('produk_id');
+          $table->bigInteger('qty_stok');
+          $table->timestamps();
+          $table->softDeletes();
+      });
+      Schema::table('stok', function (Blueprint $table) {
+          $table->index('produk_id');
+          $table->foreign('produk_id')->references('id')->on('produk')->onUpdate('cascade')->onDelete('cascade');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('stok', function (Blueprint $table) {
+          Schema::dropIfExists('stok');
+      });
+    }
+}
