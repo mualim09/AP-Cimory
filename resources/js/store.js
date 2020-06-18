0499487101958003e9886451d5facba916c7ca3f
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-18 09:26:20 +0700

    tamba state dan localStorage kode_dc

--- resources/js/store.js
+++ resources/js/store.js
@@ -6,7 +6,8 @@ export const store = new Vuex.Store({
   state: {
     username: localStorage.getItem('username') || null,
     role: localStorage.getItem('role') || null,
-    token: localStorage.getItem('token') || null
+    token: localStorage.getItem('token') || null,
+    dc: localStorage.getItem('dc') || null
   },
   mutations: {
     setUsername (state, username) {
@@ -26,6 +27,12 @@ export const store = new Vuex.Store({
     },
     destroyToken (state) {
       state.token = null
+    },
+    setDC (state, dc) {
+      state.dc = dc
+    },
+    destroyDC (state) {
+      state.dc = null
     }
   },
   actions: {
@@ -33,21 +40,26 @@ export const store = new Vuex.Store({
       let username = payload.username
       let role = payload.role
       let token = payload.token
+      let dc = payload.dc
       localStorage.setItem('username', username)
       localStorage.setItem('role', role)
       localStorage.setItem('token', token)
+      localStorage.setItem('dc', dc)
       context.commit('setUsername', username)
       context.commit('setRole', role)
       context.commit('setToken', token)
+      context.commit('setDC', dc)
     },
-    
+
     hapusState (context) {
       localStorage.removeItem('username')
       localStorage.removeItem('role')
       localStorage.removeItem('token')
+      localStorage.removeItem('dc')
       context.commit('destroyUsername')
       context.commit('destroyRole')
       context.commit('destroyToken')
+      context.commit('destroyDC')
     }
   },
   getters: {
