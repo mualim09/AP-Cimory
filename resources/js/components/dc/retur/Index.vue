ea790f630f7f178f3f3c2ac065bbe4143cc4230c
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-18 09:25:23 +0700

    perbarui tabel laporan retur

---
+++ resources/js/components/dc/retur/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-col cols lg="10" xs=12>
+    <div class="login-form mx-auto text-center">
+      <h2>Laporan Retur</h2>
+      <v-client-table :data="tableRetur" :columns="columns" :options="options">
+        <!-- <router-link :to="{name: 'edit-dc', params: { id: props.row.id }}" slot="edit" slot-scope="props"><b-button size="sm" variant="outline-primary">Edit</b-button></router-link>
+        <b-button size="sm" variant="outline-danger" @click="deleteDC(props.row.id)"  slot="hapus" slot-scope="props">Delete</b-button> -->
+      </v-client-table>
+    </div>
+  </b-col>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        dataRetur: [],
+        columns: ['dc.kode_dc', 'tanggal_sjr', 'kode_sjr', 'produk.nama_produk', 'qty_retur'],
+        tableRetur: [],
+        options: {
+          perPage: 100,
+          headings: {
+            'dc.kode_dc': 'Kode DC',
+            tanggal_sjr: 'Tanggal SJR',
+            kode_sjr: 'Kode SJR',
+            'produk.nama_produk': 'Produk',
+            qty_retur: 'Qty Retur',
+          },
+          sortable: ['kode_dc', 'tanggal_sjr', 'kode_sjr', 'produk.nama_produk', 'qty_retur']
+        }
+      }
+    },
+    created() {
+      this.axios
+        .get(`/api/retur/${localStorage.getItem('dc')}`)
+        .then(response => {
+          this.dataRetur = response.data
+          this.tableRetur = response.data
+        })
+    },
+    // methods: {
+    //   deleteDC(transaksi) {
+    //     this.axios
+    //       .delete(`/api/transaksi/delete/${id}`)
+    //       .then(response => {
+    //         let i = this.dataTransaksi.map(item => item.id).indexOf(id); // find index of your object
+    //         this.dataTransaksi.splice(i, 1)
+    //       })
+    //   }
+    // }
+  }
+</script>
