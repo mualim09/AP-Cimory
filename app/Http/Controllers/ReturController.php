186ed622e2a8f4f1b1704c7964d04d6da61c1c9b
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-20 23:04:54 +0700

    update watch

--- app/Http/Controllers/ReturController.php
+++ app/Http/Controllers/ReturController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Retur;
 use App\Pembelian;
 use App\Stok;
+use App\Laporan;
 
 class ReturController extends Controller
 {
@@ -36,6 +37,34 @@ class ReturController extends Controller
         }
     }
 
+    public function update_laporan($dc_id, $tanggal_sjr, $produk_id, $qty_retur)
+    {
+        // Cari data laporan yang akan diupdate
+        $laporan = Laporan::where('dc_id', $dc_id)->where('produk_id', $produk_id)->orderBy('created_at', 'desc')->first();
+        // Jika tanggal laporan terakhir/yang ada == tanggal_penjualan, update stok laporan tersebut. Berarti pembelian penjualan dan retur dihari yang sama
+        if ($laporan->tanggal == $tanggal_sjr) {
+          $laporan->qty_retur = $qty_retur;
+          $laporan->qty_stok = $laporan->qty_pembelian - $laporan->qty_penjualan - $qty_retur;
+          if ($laporan->save()) {
+            return true;
+          }
+        } else {
+        $stok = $laporan->qty_stok;
+        // Jika tanggal laporan terakhir != tanggal_penjualan, buat row laporan baru, artinya tidak ada pembelian dan penjualan tapi ada retur
+        $new_laporan = new Laporan([
+          'dc_id' => $dc_id,
+          'tanggal' => $tanggal_sjr,
+          'produk_id' => $produk_id,
+          'qty_pembelian' => 0,
+          'qty_penjualan' => 0,
+          'qty_stok' => number_format($laporan->qty_stok) - number_format($qty_retur)
+        ]);
+        if ($new_laporan->save()) {
+          return true;
+        }
+        }
+    }
+
     public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -48,6 +77,7 @@ class ReturController extends Controller
 
         $retur = new Retur($request->all());
         if ($this->update_stok($dc_id = $retur['dc_id'], $produk_id = $retur['produk_id'], $qty_retur = $retur['qty_retur'], $tanggal_sjr = $retur['tanggal_sjr'], $kode_sjr = $retur['kode_sjr'])) {
+            $this->update_laporan($dc_id = $retur['dc_id'], $produk_id = $retur['produk_id'], $qty_retur = $retur['qty_retur'], $tanggal_sjr = $retur['tanggal_sjr'], $kode_sjr = $retur['kode_sjr']);
             $retur->save();
             return response()->json("Sukses ".$retur['kode_transaksi']);
         } else {
