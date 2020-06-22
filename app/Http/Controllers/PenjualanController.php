caaa0c3afb8c209e9eb4189a7d20733ec9ec6cc3
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-22 14:06:44 +0700

    keterangan stok dengan qty, $laporan belum terpanggil di if else

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -25,12 +25,13 @@ class PenjualanController extends Controller
     {
         // Cari stok terakhir untuk dikurangi qty_penjualan dan disimpan di tabel stok
         $stok_terakhir = Stok::where('dc_id', $dc_id)->where('produk_id', $produk_id)->orderBy('created_at', 'desc')->first();
+        // Stok terakhir seharusnya tidak null karena pembelian pertama kali menghasilkan stok => PembelianController => 67
         if ($stok_terakhir != null) {
           $qty_stok = $stok_terakhir->qty_stok - $qty_penjualan;
           $stok = new Stok([
               'dc_id' => $dc_id,
               'tanggal_stok' => $tanggal_penjualan,
-              'keterangan' => "penjualan ".$kode_transaksi,
+              'keterangan' => "penjualan ".$kode_transaksi." : ".$qty_penjualan,
               'produk_id' => $produk_id,
               'qty_stok' => $qty_stok,
           ]);
@@ -42,22 +43,13 @@ class PenjualanController extends Controller
     public function update_laporan($dc_id, $tanggal_penjualan, $produk_id, $qty_penjualan)
     {
         // Cari data laporan yang akan diupdate
-        // $laporan = Laporan::where('dc_id', $dc_id)->where('tanggal', $tanggal_penjualan)->where('produk_id', $produk_id)->first();
         $laporan = Laporan::where('dc_id', $dc_id)->where('produk_id', $produk_id)->orderBy('created_at', 'desc')->first();
         // Jumlahkan semua nilai penjualan berdasarkan dc, tanggal dan produk
         $sum_penjualan = Penjualan::where('dc_id', $dc_id)->where('tanggal_penjualan', $tanggal_penjualan)->where('produk_id', $produk_id)->sum('qty_penjualan');
         // Jika tanggal laporan terakhir/yang ada == tanggal_penjualan, update stok laporan tersebut. Berarti pembelian dan penjualan dihari yang sama
         if ($laporan->tanggal == $tanggal_penjualan) {
-          // $laporan->update(array('qty_penjualan' => $sum_penjualan, 'qty_stok' => $laporan->qty_pembelian - $sum_penjualan));
-          // $laporan->qty_stok = $laporan->qty_pembelian - $sum_penjualan;
+          // Bug, $laporan tidak bisa akses didalam if else, sehingga buat variable baru $laporan1 dan $laporan2
           $laporan1 = Laporan::where('dc_id', $dc_id)->where('produk_id', $produk_id)->orderBy('created_at', 'desc')->first();
-
-          // $laporan->update(array('qty_penjualan' => $sum_penjualan, 'qty_stok' => $laporan->qty_pembelian - $sum_penjualan)));
-          // if ($laporan1->qty_pembelian == 0) {
-          //   $update_laporan = Laporan::where('id', $laporan1->id)->update(array('qty_penjualan' => $sum_penjualan, 'qty_stok' => $laporan1->qty_stok - $qty_penjualan));
-          // } else {
-          //   $update_laporan = Laporan::where('id', $laporan1->id)->update(array('qty_penjualan' => $sum_penjualan, 'qty_stok' => $laporan1->qty_stok - $sum_penjualan));
-          // }
           $update_laporan = Laporan::where('id', $laporan1->id)->update(array('qty_penjualan' => $sum_penjualan, 'qty_stok' => $laporan1->qty_stok - $qty_penjualan));
         } else {
         // Jika tanggal laporan terakhir != tanggal_penjualan, buat row laporan baru, artinya tidak ada pembelian tapi ada penjualan
@@ -71,11 +63,7 @@ class PenjualanController extends Controller
             'qty_retur' =>0,
             'qty_stok' => $laporan2->qty_stok - $sum_penjualan
           ]);
-          if ($new_laporan->save()) {
-            return true;
-          } else {
-            return false;
-          }
+          $new_laporan->save();
         }
     }
 
@@ -95,7 +83,6 @@ class PenjualanController extends Controller
         if ($penjualan->save()) {
             $this->update_stok($dc_id = $penjualan['dc_id'], $tanggal_penjualan = $penjualan['tanggal_penjualan'], $kode_transaksi = $penjualan['kode_transaksi'], $produk_id = $penjualan['produk_id'], $qty_penjualan = $penjualan['qty_penjualan']);
             $this->update_laporan($dc_id = $penjualan['dc_id'], $tanggal_penjualan = $penjualan['tanggal_penjualan'], $produk_id = $penjualan['produk_id'], $qty_penjualan = $penjualan['qty_penjualan']);
-            // $this->update_laporan($dc_id = $penjualan['dc_id'], $tgl_penjualan = $penjualan['tanggal_penjualan'], $produk_id = $penjualan['produk_id'], $qty_penjualan = $penjualan['qty_penjualan']);
             return response()->json("Sukses ".$penjualan['kode_transaksi']);
         } else {
             return response()->json("Harap periksa input anda, pastikan tanggal penjualan = tanggal pembelian produk tersebut");
