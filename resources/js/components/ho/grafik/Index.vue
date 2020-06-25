74f946679609f7606b7a2b1dc07f36565f2b86c1
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-06-25 21:01:22 +0700

    fix container chart

--- resources/js/components/ho/grafik/Index.vue
+++ resources/js/components/ho/grafik/Index.vue
@@ -1,15 +1,16 @@
+<template>
   <b-col cols lg="10" xs=12>
     <b-row>
-      <form @submit.prevent="createPembelian">
+      <form @submit.prevent="cariGrafik">
 
         <div class="form-group row">
-          <label for="tanggal_pencarian" class="col-md-4 col-form-label text-md-right">Tanggal Pencarian</label>
+          <label for="tanggal" class="col-md-4 col-form-label text-md-right">Tanggal Pencarian</label>
           <div class="col-md-6">
-            <input id="tanggal_pencarian" type="text" class="form-control" v-model="pembelian.no_invoice">
+            <input id="tanggal" type="date" class="form-control" v-model="tanggal">
           </div>
         </div>
 
-        <div class="form-group row">
+        <!-- <div class="form-group row">
           <label for="produk_id" class="col-md-4 col-form-label text-md-right">Produk</label>
           <div class="col-md-6">
             <select v-model="pembelian.produk_id" class="form-control">
@@ -19,51 +20,37 @@
               </option>
             </select>
           </div>
-        </div>
+        </div> -->
 
-        <b-button type="submit" variant="primary">Simpan</b-button>
+        <b-button type="submit" variant="primary">Tampilkan</b-button>
 
       </form>
     </b-row>
     <b-row>
       <div class="login-form mx-auto text-center">
         <h2>Grafik</h2>
-          <div
+        <bar-chart
           v-if="loaded"
-          :chartdata="chartData"
-          :options="options">
-        </div>
+          :chartData="chartData"
+          :options="options"/>
       </div>
     </b-row>
   </b-col>
-
+</template>
 <script>
-import VueCharts from 'vue-chartjs'
-import { Bar, mixins } from 'vue-chartjs'
-//  const { reactiveProp } = mixins
+import BarChart from './BarChart.vue'
 
   export default {
-    extends: Bar,
-    mixins: [mixins.reactiveData],
-    //props: ['options'],
+    name: 'BarChartContainer',
+    components: { BarChart },
     data() {
       return {
-        chartData: '',
-
         loaded: false,
-        // chartdata: {
-        //   labels: ['January', 'February'],
-        //   datasets: [
-        //       {
-        //         label: 'Data One',
-        //         backgroundColor: '#f87979',
-        //         pointBackgroundColor: 'white',
-        //         borderWidth: 1,
-        //         pointBorderColor: '#249EBF',
-        //         data: [40, 20]
-        //       }
-        //     ]
-        // },
+        tanggal: null,
+        chartData: {
+          },
+        pembelian: {},
+        cari_produk: [],
         options: {
           scales: {
             yAxes: [{
@@ -88,50 +75,82 @@ import { Bar, mixins } from 'vue-chartjs'
         }
       }
     },
-    created() {
-      // Cari data
-      this.axios
-        .get('api/grafik_pembelian/2020-06-18/1')
-        .then(response => (
-          console.log(response.data),
-          // responseData = response.data,
-          this.chartData = {
-            labels: response.data.map(item => item.dc.kode_dc),
-            datasets: [
-              {
-                label: 'Grafik Pembelian',
-                backgroundColor: '#f87979',
-                data: response.data.map(item => item.qty_pembelian)
-              }
-            ]
-          },
-          console.log(this.chartdata),
-          this.loaded = true
-        ))
-        .catch(error => console.log(error))
-        .finally(() => this.loading = false)
-      // Cari produk
+    mounted (){
+
+      },
+    async created() {
       this.axios
         .get('api/cari_produk')
         .then(response => (
-          console.log(response.data),
           this.cari_produk = response.data
         ))
         .catch(error => console.log(error))
         .finally(() => this.loading = false)
+
+        var self = this
+        this.loaded = false
+        new Promise(function (resolve, reject){
+          self.axios
+            .get(`/api/grafik_penjualan`, { params: {tanggal: self.tanggal }})
+            .then(response => (
+              self.chartData = {
+                labels: response.data.map(item => item.kode_dc),
+                datasets: [
+                  {
+                    label: `Pembelian ${response.data[0].nama_produk}`,
+                    backgroundColor: '#38c172',
+                    data: response.data.map(item => item.pembelian),
+                  },
+                  {
+                    label: `Penjualan ${response.data[0].nama_produk}`,
+                    backgroundColor: '#3f9ae5',
+                    data: response.data.map(item => item.penjualan),
+                  }
+                ]
+              }
+            ))
+            .catch(error => console.log(error))
+          resolve (self.loaded = true)
+        })
+        .catch(error => console.log(error))
     },
-    mounted () {
-        this.renderChart(this.chartData, this.options)
+    method: {
+      cariGrafik() {
+        this.axios
+          .get('api/cari_produk')
+          .then(response => (
+            this.cari_produk = response.data
+          ))
+          .catch(error => console.log(error))
+          .finally(() => this.loading = false)
+
+          var self = this
+          this.loaded = false
+          new Promise(function (resolve, reject){
+            self.axios
+              .get(`/api/grafik_penjualan`, { params: {tanggal: this.tanggal }})
+              .then(response => (
+                self.chartData = {
+                  labels: response.data.map(item => item.kode_dc),
+                  datasets: [
+                    {
+                      label: `Pembelian ${response.data[0].nama_produk}`,
+                      backgroundColor: '#38c172',
+                      data: response.data.map(item => item.pembelian),
+                    },
+                    {
+                      label: `Penjualan ${response.data[0].nama_produk}`,
+                      backgroundColor: '#3f9ae5',
+                      data: response.data.map(item => item.penjualan),
+                    }
+                  ]
+                }
+              ))
+              .catch(error => console.log(error))
+            resolve (self.loaded = true)
+          })
+          .catch(error => console.log(error))
+      }
     }
-    // methods: {
-    //   deleteDC(transaksi) {
-    //     this.axios
-    //       .delete(`/api/transaksi/delete/${id}`)
-    //       .then(response => {
-    //         let i = this.dataTransaksi.map(item => item.id).indexOf(id); // find index of your object
-    //         this.dataTransaksi.splice(i, 1)
-    //       })
-    //   }
-    // }
   }
 </script>
